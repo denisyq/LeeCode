6c1c8493f7f17d7f765176b4b3c289154a4911c5
denisyq <denisyq@gmail.com>
2016-08-25 10:22:40 +0800

    Update 217_Contains_Duplicate.cc

--- 217_Contains_Duplicate.cc
+++ 217_Contains_Duplicate.cc
@@ -1,4 +1,4 @@
-class Solution{
+/*class Solution{//71ms
 public:
     bool containsDuplicate(vector<int>& nums){
         set<int> iset;
@@ -8,6 +8,18 @@ public:
             if(ret.second == false){
                 return true;
             }
+        }
+        return false;
+    }
+};*/
+class Solution{//40ms
+public:
+    bool containsDuplicate(vector<int>& nums){
+        if(nums.size()<=1) return false;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i] != nums[i+1]) continue;
+            else return true;
         }
         return false;
     }
